d9aee0af2a8f8e33738039f365fecc3152ffca9a
Florian Wagner <slvwagner@gmail.com>
2023-09-05 06:43:48 +0200

    generate table of content fist running version

--- tabel of contents for Markdown file.R
+++ tabel of contents for Markdown file.R
@@ -1,19 +1,49 @@
 library(rebus)
 library(stringr)
 
-contents_tbl <- function(Rmd_doc) {
+contents_tbl <- function(FileName) {
   # read markdown file 
-  c_Rmd <- readLines("input/test.Rmd")
+  c_Rmd <- suppressWarnings(readLines(FileName))
   c_Rmd
+  
   # regex search pattern 
   p <- c(START%R%"#")
   
-  # put ancor to headings 
-  c_Rmd <- ifelse(str_detect(c_Rmd, p), paste0(c_Rmd, "<a name=\"",c_Rmd,"\"></a>"), c_Rmd)
-  c_Rmd
+  # find positon to insert tabel of contents
+  for(ii in 1:length(c_Rmd)){
+    if(str_detect(c_Rmd[ii],p)) {
+      c_start <- ii
+      break
+      }
+  }
   
+  # Headings to find
   heading <- c_Rmd[str_detect(c_Rmd, p)]|>str_remove_all("#")|>str_split("<", simplify = T)
   heading <- heading[,1]|>str_trim()
-  paste0("[",heading,"]","(#",heading,")")
-  return(c_md)
+  
+  # put anchor to headings 
+  c_Rmd <- ifelse(str_detect(c_Rmd, p), paste0(c_Rmd, "<a name=\"",c_Rmd|>str_remove_all("#")|>str_trim(),"\"></a>"), c_Rmd)
+  c_Rmd
+
+  # create table on contents
+  c_Rmd <- c(c_Rmd[1:(c_start-1)],
+            "\n# Tabel of Content",
+            paste0("[",heading,"]","(#",heading,")","  \\"), "\n", # insert table of contents
+            c_Rmd[c_start:length(c_Rmd)])
+  return(c_Rmd)
 }
+
+c_FileName <- "test"
+c_Rmd <- contents_tbl(paste0("input/",c_FileName, ".Rmd"))
+c_Rmd
+
+# write created markdown file  
+c_Rmd|>
+  write(paste0("output/",c_FileName, ".Rmd"))
+
+# render html file 
+rmarkdown::render(paste0("output/",c_FileName,".Rmd"), 
+                  c("html_document"))
+
+# show html in web browser
+browseURL(paste0("output/",c_FileName,".html"))
